00acbd38f5986810b5e81435a1d23a10fb756782
BaoBao112233 <kevinbao15072002@gmai.com>
2024-08-24 18:06:36 +0700

    update

--- get_data.py
+++ get_data.py
@@ -10,43 +10,68 @@ from selenium.webdriver.chrome.options import Options
 from anticaptchaofficial.recaptchav2proxyless import *
 from webdriver_manager.chrome import ChromeDriverManager
 
-print("Task 1: Đăng nhập vào TopCV")
-# Mở trình duyệt, vào trang TopCV
+def login_web(url):
+    print("Task 1: Đăng nhập vào TopCV")
+    # Mở trình duyệt, vào trang TopCV
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-url = 'https://www.topcv.vn/viec-lam'
-driver.get(url)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver.get(url)
+    return driver
 
-print("Task 2: Tìm kiếm nội dụng trên thanh search box")
-search_box = driver.find_element(By.ID, "keyword") # #keyword
-search_box.send_keys("Software Engineer")# input("Nhập nghề nghiệp, vị trí muốn tìm: "))
-time.sleep(2)
-search_box.send_keys(Keys.RETURN)
+def find_search_box(driver, attribute_elm, name, key)
+    print("Task 2: Tìm kiếm nội dụng trên thanh search box")
+    search_box = driver.find_element(attribute_elm, name) # #keyword
+    search_box.send_keys(key)
+    time.sleep(2)
+    search_box.send_keys(Keys.RETURN)
 
-print("Task 3: Mở url của các công ty cần tuyển dụng")
-
-try:
-    # Tìm thẻ div cụ thể bằng class name (thay 'ten-class-cua-ban' bằng tên class bạn cần)
-    div_element = driver.find_element(By.CLASS_NAME, "job-list-search-result")
+def get_data(url);
+    response = requests.get(url)
     
-    # Tìm tất cả các thẻ <a> trong thẻ <div> đó
-    anchor_tags = div_element.find_elements(By.TAG_NAME, "a")
+    # Kiểm tra nếu yêu cầu thành công (HTTP status code 200)
+    if response.status_code == 200:
+        # Tạo đối tượng BeautifulSoup
+        soup = BeautifulSoup(response.content, 'lxml')  # hoặc 'html.parser'
+        
+        
+    else:
+        print(f"Yêu cầu không thành công` với mã trạng thái: {response.status_code}")
 
-    # Lọc các thẻ <a> có thuộc tính href
-    anchor_tags_with_href = [a for a in anchor_tags if a.get_attribute("href")]
 
-    print(f"Tìm thấy {len(anchor_tags_with_href)} thẻ <a> có thuộc tính href trong thẻ <div> có class 'job-list-search-result' đã chọn.")
-    list_post = [tag_a.get_attribute('href') for tag_a in anchor_tags_with_href]
-    # for index, post in enumerate(anchor_tags_with_href):
-    #     print(f"Thẻ <a> {index + 1}: href='{post.get_attribute('href')}', text='{post.text}'")
+def save_data(driver, attribute_elm, name):
+    print("Task 3: Mở url của các công ty cần tuyển dụng")
 
-except Exception as e:
-    print("Không tìm thấy thẻ div hoặc thẻ a phù hợp:", e)
+    try:
+        # Tìm thẻ div cụ thể bằng class name (thay 'ten-class-cua-ban' bằng tên class bạn cần)
+        div_element = driver.find_element(attribute_elm, name)
+        
+        # Tìm tất cả các thẻ <a> trong thẻ <div> đó
+        anchor_tags = div_element.find_elements(By.TAG_NAME, "a")
 
-while True:
-    pass
+        # Lọc các thẻ <a> có thuộc tính href
+        anchor_tags_with_href = [a for a in anchor_tags if a.get_attribute("href")]
+
+        print(f"Tìm thấy {len(anchor_tags_with_href)} thẻ <a> có thuộc tính href trong thẻ <div> có class 'job-list-search-result' đã chọn.")
+        list_post = [tag_a.get_attribute('href') for idx, tag_a in enumerate(anchor_tags_with_href) if idx <= 10]
+        # for index, post in enumerate(anchor_tags_with_href):
+        #     print(f"Thẻ <a> {index + 1}: href='{post.get_attribute('href')}', text='{post.text}'")
+        
+        for post in list_post:
+            response = requests.get(post)
+
+            print(post)
+
+
+    except Exception as e:
+        print("Không tìm thấy thẻ div hoặc thẻ a phù hợp:", e)
+
+def main():
+    url = 'https://www.topcv.vn/viec-lam'
+    driver = login_web(url)
+    find_search_box(driver, By.ID, 'keyword', "Software Engineer")
+    save_data(driver, By.CLASS_NAME, 'job-list-search-result')
 
 # Tải nội dung trang web
 # response = requests.get(url)
